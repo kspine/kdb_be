fbdec6b424b84a679246c251f6ff3ce4c7e592e9
Kylin <kylin@localhost.localdomain>
2017-06-21 18:27:51 +0800

    修改接口数据

--- mock.py
+++ mock.py
@@ -5,43 +5,57 @@ all shop list
 all book list
 """
 
-datatype_list = ['测试', '售价', '折扣', '销量', '评论数', '直通车投入']
-shop_list = ['测试', '人民邮电出版社官方旗舰店', '新华文轩', '当当网']
+datatype_list = [
+    {'id':'测试', 'text':'测试'},
+    {'id':'售价', 'text':'售价'},
+    {'id':'折扣', 'text':'折扣'},
+    {'id':'销量', 'text':'销量'},
+    {'id':'评论数', 'text':'评论数'},
+    {'id':'直通车投入', 'text':'直通车投入'}
+]
+
+shop_list = [
+    {'id':'测试', 'text':'测试'},
+    {'id':'测试', 'text':'测试'},
+    {'id':'人邮', 'text':'人邮'},
+    {'id':'新华', 'text':'新华'},
+    {'id': '当当', 'text': '当当网'}
+]
+
+# 没有单独的book_list
 book_list = [
     {
-        'isbn': '',
-        'name': '测试'
+        'id': '',
+        'text': '测试'
     },
     {
-        'isbn': '',
-        'name': '软件工程'
+        'id': '',
+        'text': '软件工程'
     },
     {
-        'isbn': '',
-        'name': '操作系统'
+        'id': '',
+        'text': '操作系统'
     },
     {
-        'isbn': '',
-        'name': '计算机网络'
+        'id': '',
+        'text': '计算机网络'
     }
 ]
 
 
-mb = {
+data = {
     'type': '销量',
     'data': [
         # 一个宫格, 某店某书某指标的走势图
         {
-            'shop': '人邮',
-            'isbn': '12345678',
-            'book': 'python',
+            'shop': {'id': '人邮', 'text':'人邮'},
+            'book': {'id': 'python', 'text': 'python'},
             'date': ['2017/06/10', '2017/06/11', '2017/06/12', '2017/06/13', '2017/06/14', ],
             'val': [8, 3, 9, 8, 3, 9],
         },
         {
-            'shop': '人邮',
-            'isbn': '12345678',
-            'book': 'c++',
+            'shop': {'id': '人邮', 'text':'人邮'},
+            'book': {'id': 'c++', 'text': 'c++'},
             'date': ['2017/06/11', '2017/06/12', '2017/06/13', '2017/06/14', '2017/06/15', ],
             'val': [8, 3, 9, 8, 3, 9],
         }
@@ -52,60 +66,56 @@ query = {
     'type': '',
     'data': [
         {
-            'shop': '',
-            'isbn': '',
-            'book': '',
-        }
-        ,
+            'shop': {'id': '', 'text': '人邮'},
+            'book': {'id': '软件工程', 'text': '软件工程'}
+        },
         {
-            'shop': '',
-            'isbn': '',
-            'book': '',
+            'shop': {'id': '', 'text': '人邮'},
+            'book': {'id': '软件工程', 'text': '软件工程'}
         }
     ]
 }
 
 templ_list = [
     {
-        'name': '张三-软件工程-销量',
-        'type': '销量',
+        'id': '',
+        'text': '张三-软件工程-销量',
+        #'type': '销量',
         'data': [
             {
-                'shop': '人邮',
-                'isbn': '12345678',
-                'book': '软件工程'
+                'shop': {'id': '', 'text': '人邮'},
+                'book': {'id': '软件工程', 'text': '软件工程'}
             }
         ]
     },
     {
-        'name': '李四-软件工程-销量',
-        'type': '销量',
+        'id': '',
+        'text': '李四-软件工程-销量',
+        #'type': '销量',
         'data': [
             {
-                'shop': '当当网',
-                'isbn': '12345678',
-                'book': '操作系统'
+                'shop': {'id': '', 'text': '人邮'},
+                'book': {'id': '软件工程', 'text': '软件工程'}
             }
         ]
     }
 ]
 
 init = {
-    'm_shop_book_data': mb,
+    'data': data,
     'datatype_list': datatype_list,
     'template_list': templ_list,
     'shop_list': shop_list,
-    'book_list': book_list,
+    #'book_list': book_list, # 从 data 中取
 }
 
 save_templ = {
-    'name': '李四-软件工程-销量',
-    'type': '销量',
+    'text': '李四-软件工程-销量',
+    #'type': '销量',
     'data': [
         {
-            'shop': '人邮',
-            'isbn': '12345678',
-            'book': '软件工程'
+            'shop': {'id': '', 'text':'人邮'},
+            'book': {'id': '软件工程', 'text': '软件工程'}
         }
     ]
 }
