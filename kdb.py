f6ba515c1290301e853a903dd193436ec37c5834
Kylin <kylin@localhost.localdomain>
2017-06-24 19:06:52 +0800

    backend auth, frontend jsonwebtoken

--- kdb.py
+++ kdb.py
@@ -1,5 +1,12 @@
+import jwt
 import tornado.ioloop
 import tornado.web
+import tornado.options
+import os.path
+import time
+from tornado.options import define, options
+
+define("port", default=8888, help="run on the given port", type=int)
 
 import json
 
@@ -15,18 +22,91 @@ class BaseHandler(tornado.web.RequestHandler):
         self.set_status(204)
         self.finish()
 
+    def get_current_user(self):
+        return self.get_secure_cookie("user")
+
     #def write(self, chunk):
     #    super(BaseHandler, self).write(chunk)
 
 
+# class LoginHandler(BaseHandler):
+#     def get(self):
+#         print('hello get')
+#         #self.redirect("http://localhost:4200/content")
+#         self.write("Hello, world")
+#     def post(self):
+#         param = self.request.body.decode('utf-8')
+#         param = json.loads(param)
+#         #self.write({'token': 'kylin_token'})
+#         if param['username'] == 'admin' and param['password'] == '111111':
+#             self.write({'...': '...'})
+#         else:
+#             self.write({
+#                 "request" : "/api/authenticate",
+#                 "error_code" : "20502",
+#                 "error" : "Need you follow uid."
+#             })
+
+class LoginHandler(BaseHandler):
+    @tornado.gen.coroutine
+    def get(self):
+        incorrect = self.get_secure_cookie("incorrect")
+        if incorrect and int(incorrect) > 20:
+            self.write('<center>blocked</center>')
+            return
+        self.render('login.html')
+
+    @tornado.gen.coroutine
+    def post(self):
+        incorrect = self.get_secure_cookie("incorrect")
+        if incorrect and int(incorrect) > 20:
+            self.write('<center>blocked</center>')
+            return
+
+        # getusername = tornado.escape.xhtml_escape(self.get_argument("username"))
+        # getpassword = tornado.escape.xhtml_escape(self.get_argument("password"))
+
+        param = self.request.body.decode('utf-8')
+        param = json.loads(param)
+        getusername = param['username']
+        getpassword = param['password']
+        #self.write({'token': 'kylin_token'})
+
+        if "admin" == getusername and "111111" == getpassword:
+            self.set_secure_cookie("user", getusername)
+            self.set_secure_cookie("incorrect", "0")
+            id_token = jwt.encode({'admin': '111111', 'exp':time.time()+10}, 'secret', algorithm='HS256').decode('utf8')
+            self.write({'id_token': id_token})
+            #self.redirect(self.reverse_url("main"))
+        else:
+            incorrect = self.get_secure_cookie("incorrect") or 0
+            increased = str(int(incorrect)+1)
+            self.set_secure_cookie("incorrect", increased)
+            self.write({
+                "request" : "/api/authenticate",
+                "error_code" : "20502",
+                "error" : "Need you follow uid."
+            })
+
+
+class LogoutHandler(BaseHandler):
+    def get(self):
+        self.clear_cookie("user")
+        self.redirect(self.get_argument("next", self.reverse_url("main")))
+
+
 class MainHandler(BaseHandler):
+    @tornado.web.authenticated
     def get(self):
         self.write("Hello, world")
+
+    @tornado.web.authenticated
     def post(self):
         self.write("Hello, world")
 
 
 class SaveTemplateHandler(BaseHandler):
+    @tornado.web.authenticated
     def post(self):
         import mock
 
@@ -36,29 +116,15 @@ class SaveTemplateHandler(BaseHandler):
         self.write({'res':True})
 
 
-class LoginHandler(BaseHandler):
-    def get(self):
-        print('hello get')
-        #self.redirect("http://localhost:4200/content")
-        self.write("Hello, world")
-    def post(self):
-        param = self.request.body.decode('utf-8')
-        param = json.loads(param)
-        #self.write({'token': 'kylin_token'})
-        if param['username'] == 'admin' and param['password'] == '111111':
-            self.write({'...': '...'})
-        else:
-            self.write({
-                "request" : "/api/authenticate",
-                "error_code" : "20502",
-                "error" : "Need you follow uid."
-            })
 
 class SingleBookHandler(BaseHandler):
+    @tornado.web.authenticated
     def get(self):
         print('hello get')
         #self.redirect("http://localhost:4200/content")
         self.write("Hello, world")
+
+    @tornado.web.authenticated
     def post(self):
         param = self.request.body.decode('utf-8')
         param = json.loads(param)
@@ -81,10 +147,13 @@ class MultiShopBookHandler(BaseHandler):
     all book list
     """
 
+    @tornado.web.authenticated
     def get(self):
         print('hello get')
         #self.redirect("http://localhost:4200/content")
         self.write("Hello, world")
+
+    @tornado.web.authenticated
     def post(self):
         import mock
 
@@ -98,6 +167,8 @@ class MultiShopBookHandler(BaseHandler):
 
 
 class MultiShopBookQueryHandler(BaseHandler):
+
+    @tornado.web.authenticated
     def post(self):
         import mock
 
@@ -122,6 +193,7 @@ class MultiShopBookQueryHandler(BaseHandler):
         self.write(res)
 
 class MultiShopBookSaveTemplateHandler(BaseHandler):
+    @tornado.web.authenticated
     def post(self):
         import mock
 
@@ -135,10 +207,13 @@ class MultiShopBookSaveTemplateHandler(BaseHandler):
         self.write({'id':12345})
 
 class ConcernedDataHandler(BaseHandler):
+    @tornado.web.authenticated
     def get(self):
         print('hello get')
         #self.redirect("http://localhost:4200/content")
         self.write("Hello, world")
+
+    @tornado.web.authenticated
     def post(self):
         param = self.request.body.decode('utf-8')
         param = json.loads(param)
@@ -152,8 +227,20 @@ class ConcernedDataHandler(BaseHandler):
                 "error" : "Need you follow uid."
             })
 
-def make_app():
-    return tornado.web.Application([
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        base_dir = os.path.dirname(__file__)
+        settings = {
+            "cookie_secret": "bZJc2sWbQLKos6GkHn/VB9oXwQt8S0R0kRvJ5/xJ89E=",
+            "login_url": "/login",
+            'template_path': os.path.join(base_dir, "template"),
+            'static_path': os.path.join(base_dir, "static"),
+            'debug':True,
+            #"xsrf_cookies": True,
+        }
+
+        tornado.web.Application.__init__(self, [
         (r"/", MainHandler),
         (r"/api/authenticate", LoginHandler),
         (r"/api/save_template", MultiShopBookSaveTemplateHandler),
@@ -161,9 +248,10 @@ def make_app():
         (r"/api/query_mshopbook_init_data", MultiShopBookHandler),
         (r"/api/query_mshopbook_data", MultiShopBookQueryHandler),
         (r"/api/query_concerned_data", ConcernedDataHandler),
-    ])
+    ], **settings)
+
 
 if __name__ == "__main__":
-    app = make_app()
-    app.listen(8888)
+    tornado.options.parse_command_line()
+    Application().listen(options.port)
     tornado.ioloop.IOLoop.current().start()
